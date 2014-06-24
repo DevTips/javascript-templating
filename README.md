8da31c11c3e4721351c960ca3c4011d36adc6cfd
travisneilson <travisneilson@gmail.com>
2014-06-23 22:50:01 -0700

    initial push

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ javascript-templateing
 ======================
 
 World Cup Yall! Let's learn how to make this cool name card templatized and flip out when we hover over a world-cup players name.
+
+![Preview](preview.jpg)
---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(function() {
+  playerCard();
+});
+
+
+function playerCard() {
+
+  $('.player-card-ref').each(function() {
+
+    var $this = $(this),
+        playerName = $this.text(),
+        playerNameImg = playerName.replace(/\s+/g, '-').toLowerCase(),
+        playerHeight = $this.data('height'),
+        playerWeight = $this.data('weight'),
+        playerPlace = $this.data('place');
+    
+    var template = '<div class="player-card animated flipInX">' +
+      '<img src="img/' + playerNameImg + '.jpg">' +
+      '<h4>' + playerName + '</h4>' +
+      '<ul>' +
+        '<li><strong>Height:</strong> <span>' + playerHeight + '</span></li>' +
+        '<li><strong>Weight:</strong> <span>' + playerWeight + '</span></li>' +
+        '<li><strong>Birthplace:</strong> <span>' + playerPlace + '</span></li>' +
+      '</ul>' +
+    '</div>';
+    
+    $this.append(template);
+    
+  });
+
+}
